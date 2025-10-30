4e7037db43c9084f51724dbf793ac23a88f81c93
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2025-10-29 19:04:38 -0700

    Added player statistics to layer_manager
    Implemented data gathering for ~1/2 of statistics

--- layer_manager.gd
+++ layer_manager.gd
@@ -1,7 +1,7 @@
 extends Node2D
 const room = preload("res://Scripts/room.gd")
 #the root node of each room MUST BE NAMED Root
-#                                           scene_location                                            num_liquids    Liquid Types											                            Liquid Chances                     Num Fillings   Terrain Set                                       Terrain ID					 Threshold			  Noise Scale									Num_traps      Trap Chances                                   Num Pathways          Pathway Directions                                                                                                                                           Enemy Spawnpoints     Enemy Num Goal         NPC Spawnpoints    Can Shop
+#                                           scene_location                                            num_liquids    Liquid Types											                            Liquid Chances                     Num Fillings   Terrain Set                                       Terrain ID					 Threshold			  Noise Scale									Num_traps      Trap Chances                                   Num Pathways          Pathway Directions                                                                                                                                           Enemy Spawnpoints     Enemy Num Goal         NPC Spawnpoints    Has Shop
 @onready var cave_stage : Array[Room] = [room.Create_Room("res://Scenes/test_room1.tscn",                        4,            [room.Liquid.Water,room.Liquid.Water,room.Liquid.Water,room.Liquid.Water], [.75,.25,.75,.25],                     2,              [0,0],                                      [3,4],                       [.6,1.0],            Vector2i(10,10),                              3,              [.65,.65,.65],                                6,                   [room.Direction.Up,room.Direction.Right,room.Direction.Left,room.Direction.Down,room.Direction.Down,room.Direction.Right],                                       7,                     5,                               0,   false),
 										 room.Create_Room("res://Scenes/test_room2.tscn",                        2,            [room.Liquid.Water,room.Liquid.Water],                                    [.5,.5],                               2,              [0,0],                                      [4,3],                       [.6,1.0],            Vector2i(20,20),                              2,              [.75,.25],                                    5,                   [room.Direction.Up,room.Direction.Up,room.Direction.Left,room.Direction.Down,room.Direction.Right],                                                               11,                    8,                               0,   false)
 										]
@@ -11,10 +11,30 @@ var current_room : Room
 @onready var second_layer : Array[Vector2i] = []
 var room_location : Resource 
 var room_instance
+var time_passed := 0.0
 @export var water_cells := []
 @export var lava_cells := []
 @export var acid_cells := []
 @export var blocked_cells := []
+
+#
+@export var layer_ai := [
+	0,#Rooms cleared
+	0,#Combat rooms cleared
+	0,#Time spent in last room
+	0,#Time spent in game
+	0,#Time spent in combat   		#TODO
+	0,#Damage dealt   				#TODO
+	0,#Attacks made
+	0,#Enemies defeated   			#TODO
+	0,#Shops visited
+	0,#Liquid rooms visited
+	0,#Trap rooms visited
+	0,#Damage taken   				#TODO
+	0,#Elite enemies defeated   	#TODO
+	0,#Currency collected   		#TODO
+	0,#Items picked up   			#TODO
+	]
 
 
 func _ready() -> void:
@@ -28,11 +48,13 @@ func _ready() -> void:
 	#cull shop spawners
 	_floor_noise()
 	_calculate_cell_arrays()
-	
-func _process(_delta: float) -> void:
+	player.attack_requested.connect(_on_player_attack)
+func _process(delta: float) -> void:
+	time_passed += delta
 	if(Input.is_action_just_pressed("Activate")):
 		var direction = check_pathways()
 		if direction != -1:
+			_update_ai_array()
 			room_instance.queue_free()
 			second_layer = []
 			while find_child("Root") != null:
@@ -50,7 +72,38 @@ func _process(_delta: float) -> void:
 				
 					
 				
-				
+func _update_ai_array() -> void:
+	#Rooms cleared
+	layer_ai[0] += 1
+	#Combat rooms cleared
+	if !current_room.has_shop:
+		layer_ai[1] += 1
+	#Last room time
+	layer_ai[2] = time_passed - layer_ai[3]
+	#Total time
+	layer_ai[3] = time_passed
+	if current_room.has_shop:
+		layer_ai[7] += 1
+	if current_room.num_liquid > 0:
+		var liquid_num = 0
+		var liquid_type : String
+		while liquid_num < current_room.num_liquid:
+			liquid_num+=1
+			liquid_type= _get_liquid_string(current_room.liquid_types[liquid_num-1])
+			if room_instance.get_node_or_null(liquid_type+str(liquid_num)):
+				layer_ai[9] += 1   #Liquid room
+				break
+	if current_room.num_trap > 0:
+		var trap_num = 0
+		while trap_num < current_room.num_trap:
+			trap_num+=1
+			if room_instance.get_node_or_null("Trap"+str(trap_num)):
+				layer_ai[10] += 1   #Trap room
+				break
+
+	print(layer_ai)
+
+
 func check_pathways() -> int:
 	var targets_extents: Array = []
 	var targets_position: Array = []
@@ -63,25 +116,25 @@ func check_pathways() -> int:
 		match p_direct:
 			room.Direction.Left:
 				L+=1
-				if room_instance.get_node("PathwayL"+str(L)+"_Detect"):
+				if room_instance.get_node_or_null("PathwayL"+str(L)+"_Detect"):
 					targets_extents.append(room_instance.get_node("PathwayL"+str(L)+"_Detect/Area2D/CollisionShape2D").shape.extents)
 					targets_position.append(room_instance.get_node("PathwayL"+str(L)+"_Detect/Area2D/CollisionShape2D").global_position)
 					targets_id.append("PathwayL"+str(L)+"_Detect")
 			room.Direction.Right:
 				R+=1
-				if room_instance.get_node("PathwayR"+str(R)+"_Detect"):
+				if room_instance.get_node_or_null("PathwayR"+str(R)+"_Detect"):
 					targets_extents.append(room_instance.get_node("PathwayR"+str(R)+"_Detect/Area2D/CollisionShape2D").shape.extents)
 					targets_position.append(room_instance.get_node("PathwayR"+str(R)+"_Detect/Area2D/CollisionShape2D").global_position)
 					targets_id.append("PathwayR"+str(R)+"_Detect")
 			room.Direction.Down:
 				D+=1
-				if room_instance.get_node("PathwayD"+str(D)+"_Detect"):
+				if room_instance.get_node_or_null("PathwayD"+str(D)+"_Detect"):
 					targets_extents.append(room_instance.get_node("PathwayD"+str(D)+"_Detect/Area2D/CollisionShape2D").shape.extents)
 					targets_position.append(room_instance.get_node("PathwayD"+str(D)+"_Detect/Area2D/CollisionShape2D").global_position)
 					targets_id.append("PathwayD"+str(D)+"_Detect")
 			room.Direction.Up:
 				U+=1
-				if room_instance.get_node("PathwayU"+str(U)+"_Detect"):
+				if room_instance.get_node_or_null("PathwayU"+str(U)+"_Detect"):
 					targets_extents.append(room_instance.get_node("PathwayU"+str(U)+"_Detect/Area2D/CollisionShape2D").shape.extents)
 					targets_position.append(room_instance.get_node("PathwayU"+str(U)+"_Detect/Area2D/CollisionShape2D").global_position)
 					targets_id.append("PathwayU"+str(U)+"_Detect")
@@ -126,60 +179,71 @@ func _choose_pathways(direction) -> void:
 	match direction:
 		room.Direction.Left:
 			pathway = int(randf()*R)+1
-			room_instance.get_node("PathwayR"+str(pathway)).queue_free()
+			_open_pathway("PathwayR"+str(pathway))
 			player.position = room_instance.get_node("PathwayR"+str(pathway)+"_Detect").position
 			room_instance.get_node("PathwayR"+str(pathway)+"_Detect").used = true
 		room.Direction.Right:
 			pathway = int(randf()*L)+1
-			room_instance.get_node("PathwayL"+str(pathway)).queue_free()
+			_open_pathway("PathwayL"+str(pathway))
 			player.position = room_instance.get_node("PathwayL"+str(pathway)+"_Detect").position
 			room_instance.get_node("PathwayL"+str(pathway)+"_Detect").used = true
 		room.Direction.Up:
 			pathway = int(randf()*D)+1
-			room_instance.get_node("PathwayD"+str(pathway)).queue_free()
+			_open_pathway("PathwayD"+str(pathway))
 			player.position = room_instance.get_node("PathwayD"+str(pathway)+"_Detect").position
 			room_instance.get_node("PathwayD"+str(pathway)+"_Detect").used = true
 		room.Direction.Down:
 			pathway = int(randf()*U)+1
-			room_instance.get_node("PathwayU"+str(pathway)).queue_free()
+			_open_pathway("PathwayU"+str(pathway))
 			player.position = room_instance.get_node("PathwayU"+str(pathway)+"_Detect").position
 			room_instance.get_node("PathwayU"+str(pathway)+"_Detect").used = true
-	
-	#Choose which pathways to keep      #add intelligent pathway choosing #TODO
-	L = 0
-	R = 0
-	D = 0
-	U = 0
-	for p_direct in current_room.pathway_direction:
-		match p_direct:
+	#Open a random pathway
+	var dir = current_room.pathway_direction[int(randf()*current_room.num_pathways)]
+	var offset = 0
+	if dir == current_room.invert_direction(direction):
+		match direction:
 			room.Direction.Left:
-				L+=1
-				if room_instance.get_node("PathwayL"+str(L)):
-					if randf() > .5:
-						room_instance.get_node("PathwayL"+str(L)).queue_free()
-					else:
-						second_layer+=room_instance.get_node("PathwayL"+str(L)).get_used_cells()
+				if L > 1:
+					while true:
+						if room_instance.get_node_or_null("PathwayL"+str(offset+1)):
+							_open_pathway("PathwayL"+str(offset+1))
+							break
+						offset+=1
+				else:
+					_open_random_pathway_in_direction(room.Direction.Right, L, R, D, U)
 			room.Direction.Right:
-				R+=1
-				if room_instance.get_node("PathwayR"+str(R)):
-					if randf() > .5:
-						room_instance.get_node("PathwayR"+str(R)).queue_free()
-					else:
-						second_layer+=room_instance.get_node("PathwayR"+str(R)).get_used_cells()
+				if R > 1:
+					while true:
+						if room_instance.get_node_or_null("PathwayR"+str(offset+1)):
+							_open_pathway("PathwayR"+str(offset+1))
+							break
+						offset+=1
+				else:
+					_open_random_pathway_in_direction(room.Direction.Down, L, R, D, U)
 			room.Direction.Down:
-				D+=1
-				if room_instance.get_node("PathwayD"+str(D)):
-					if randf() > .5:
-						room_instance.get_node("PathwayD"+str(D)).queue_free()
-					else:
-						second_layer+=room_instance.get_node("PathwayD"+str(D)).get_used_cells()
+				if D > 1:
+					while true:
+						if room_instance.get_node_or_null("PathwayD"+str(offset+1)):
+							_open_pathway("PathwayD"+str(offset+1))
+							break
+						offset+=1
+				else:
+					_open_random_pathway_in_direction(room.Direction.Up, L, R, D, U)
 			room.Direction.Up:
-				U+=1
-				if room_instance.get_node("PathwayU"+str(U)):
-					if randf() > .5:
-						room_instance.get_node("PathwayU"+str(U)).queue_free()
-					else:
-						second_layer+=room_instance.get_node("PathwayU"+str(U)).get_used_cells()
+				if U > 1:
+					while true:
+						if room_instance.get_node_or_null("PathwayU"+str(offset+1)):
+							_open_pathway("PathwayU"+str(offset+1))
+							break
+						offset+=1
+				else:
+					_open_random_pathway_in_direction(room.Direction.Left, L, R, D, U)
+	else:
+		#Open at least one pathway in the given direction
+		_open_random_pathway_in_direction(dir, L, R, D, U)
+	#Choose which pathways to keep      #add intelligent pathway choosing #TODO
+	_open_random_pathways()
+	
 
 func _place_liquids() -> void:
 	#For each liquid check if you should place it and then check if there's room
@@ -233,7 +297,7 @@ func _place_enemy_spawners() -> void:
 			print("DEBUG: Layer collision removed")
 	while enemy_num > current_room.num_enemy_goal:
 		var curr_en = int(randf()*current_room.num_enemy_spawnpoints)+1
-		if room_instance.get_node("Enemy"+str(curr_en)):
+		if room_instance.get_node_or_null("Enemy"+str(curr_en)):
 			room_instance.get_node("Enemy"+str(curr_en)).queue_free()
 			print("DEBUG: Deleted enemy")
 			enemy_num-=1
@@ -308,3 +372,55 @@ func _get_liquid_string(liquid : room.Liquid) -> String:
 			return "Acid"
 	return ""
 	
+func _open_pathway(input : String) -> void:
+	print("DEBUG: Opened "+input)
+	room_instance.get_node(input).queue_free()
+
+func _open_random_pathway_in_direction(dir : room.Direction, L : int, R : int, D : int, U : int) -> void:
+	match dir:
+		room.Direction.Left:
+			_open_pathway("PathwayL"+str(int(randf()*L)+1))
+		room.Direction.Right:
+			_open_pathway("PathwayR"+str(int(randf()*R)+1))
+		room.Direction.Down:
+			_open_pathway("PathwayD"+str(int(randf()*D)+1))
+		room.Direction.Up:
+			_open_pathway("PathwayU"+str(int(randf()*U)+1))
+
+func _open_random_pathways() -> void:
+	var L = 0
+	var R = 0
+	var D = 0
+	var U = 0
+	for p_direct in current_room.pathway_direction:
+		match p_direct:
+			room.Direction.Left:
+				L+=1
+				if room_instance.get_node_or_null("PathwayL"+str(L)):
+					if randf() > .5:
+						_open_pathway("PathwayL"+str(L))
+					else:
+						second_layer+=room_instance.get_node("PathwayL"+str(L)).get_used_cells()
+			room.Direction.Right:
+				R+=1
+				if room_instance.get_node_or_null("PathwayR"+str(R)):
+					if randf() > .5:
+						_open_pathway("PathwayR"+str(R))
+					else:
+						second_layer+=room_instance.get_node("PathwayR"+str(R)).get_used_cells()
+			room.Direction.Down:
+				D+=1
+				if room_instance.get_node_or_null("PathwayD"+str(D)):
+					if randf() > .5:
+						_open_pathway("PathwayD"+str(D))
+					else:
+						second_layer+=room_instance.get_node("PathwayD"+str(D)).get_used_cells()
+			room.Direction.Up:
+				U+=1
+				if room_instance.get_node_or_null("PathwayU"+str(U)):
+					if randf() > .5:
+						_open_pathway("PathwayU"+str(U))
+					else:
+						second_layer+=room_instance.get_node("PathwayU"+str(U)).get_used_cells()
+func _on_player_attack(_new_attack : Attack) -> void:
+	layer_ai[6]+=1
