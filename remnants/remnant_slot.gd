a2e2a6ae3513159e5fe20b9d916ad57ba4a9d40d
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2025-11-18 19:06:04 -0800

    Outline Selection Update
    Selected Option now has a colored outline, but I think it made all the text low-res. Should find a solution for that

--- remnants/remnant_slot.gd
+++ remnants/remnant_slot.gd
@@ -6,9 +6,9 @@ class_name RemnantSlot
 signal slot_selected(index: int)
 
 @onready var btn_select: Button = $btn_select
-@onready var art: TextureRect = $btn_select/container/art
-@onready var name_label: Label = $btn_select/container/name_label
-@onready var desc_label: Label = $btn_select/container/description_label
+@onready var art: TextureRect = $btn_select/SubViewportContainer/SubViewport/container/art
+@onready var name_label: Label = $btn_select/SubViewportContainer/SubViewport/container/name_label
+@onready var desc_label: Label = $btn_select/SubViewportContainer/SubViewport/container/description_label
 
 func _ready():
 	btn_select.pressed.connect(_on_button_pressed)
@@ -26,5 +26,16 @@ func set_remnant(remnant: Resource) -> void:
 	else:
 		art.texture = null
 
+func outline_remnant(child: Node, color: Color = Color.ORANGE):
+	var shader = Shader.new()
+	shader.code = load("res://Shaders/outline.gdshader").code
+	var mat = ShaderMaterial.new()
+	mat.shader = shader
+	mat.set_shader_parameter("outline_color", color)
+	mat.set_shader_parameter("outline_thickness", 5.0)
+	child.material = mat
+
+
 func _on_button_pressed():
 	emit_signal("slot_selected", index)
+	outline_remnant($btn_select/TextureRect, Color.PURPLE)
