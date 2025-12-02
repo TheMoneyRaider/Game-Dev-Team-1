fe8fd647978f1c302cea2e2de573284edde437eb
Sheparry <sheparry@oregonstate.edu>
2025-12-02 13:40:40 -0800

    Singleplayer Functionality for cooldowns
    Cooldown bars work in singleplayer now.

--- Game Elements/ui/cooldown_bar_1.gd
+++ Game Elements/ui/cooldown_bar_1.gd
@@ -1,17 +1,33 @@
 extends Control
 
-@onready var mace_bar = $MaceBar
+@onready var cooldown_bar = $CooldownBar
 
 var max_cooldown = .5
 var current_cooldown = 0
+var is_covered = false
 
 func _ready() -> void:
-	mace_bar.step = max_cooldown/100
+	cooldown_bar.step = max_cooldown/100
 	
 func set_max_cooldown(cooldown_value : float) -> void:
 	max_cooldown = cooldown_value
-	mace_bar.max_value = max_cooldown
+	cooldown_bar.max_value = max_cooldown
+	cooldown_bar.step = max_cooldown/100
 
 func set_current_cooldown(cooldown_value : float) -> void:
 	current_cooldown = cooldown_value
-	mace_bar.value = current_cooldown
+	cooldown_bar.value = max_cooldown - current_cooldown
+
+func cover_cooldown():
+	if(is_covered):
+		cooldown_bar.tint_over = Color(0.443, 0.443, 0.443, 0.0)
+		is_covered = false
+	else:
+		cooldown_bar.tint_over = Color(0.443, 0.443, 0.443, 0.8)
+		is_covered = true
+
+func set_color(is_purple : bool) -> void:
+	if is_purple:
+		cooldown_bar.tint_progress = Color(0.627, 0.125, 0.941)
+	else:
+		cooldown_bar.tint_progress = Color(1.0, 0.647, 0.0)
