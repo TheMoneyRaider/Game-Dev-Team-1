20ecb44ae0b951e8902537525f348ffe3141a16f
Sheparry <sheparry@oregonstate.edu>
2025-12-02 16:01:39 -0800

    Fixed Crosshair bugs
    From what I could tell, just reverting it to how it used to be still works, and fixed the issues it was giving.

--- Game Elements/General Game/crosshair.gd
+++ Game Elements/General Game/crosshair.gd
@@ -18,15 +18,19 @@ func _process(_delta: float) -> void:
 			crosshair_direction = input_direction
 			
 	
-	var camera = get_tree().get_root().get_node("LayerManager/game_container/game_viewport/game_root/Camera2D")
-	var viewport_size = get_tree().get_root().get_node("LayerManager/game_container/game_viewport").size
-	var mouse_pos_viewport = get_tree().get_root().get_mouse_position()
-	var mouse_coords = camera.get_global_transform().affine_inverse().basis_xform(mouse_pos_viewport - Vector2(viewport_size/2))
-	var direction = (mouse_coords).normalized()
+	#var camera = get_tree().get_root().get_node("LayerManager/game_container/game_viewport/game_root/Camera2D")
+	#var viewport_size = get_tree().get_root().get_node("LayerManager/game_container/game_viewport").size
+	#var mouse_pos_viewport = get_tree().get_root().get_mouse_position()
+	#var mouse_coords = camera.get_global_transform().affine_inverse().basis_xform(mouse_pos_viewport - Vector2(viewport_size/2))
+	#var direction = (mouse_coords).normalized()
+	
+	var test_camera = get_viewport().get_camera_2d()
+	var test_mouse_coords = test_camera.get_global_mouse_position()
+	var test_direction  = (test_mouse_coords - player.global_position).normalized()
 	if player_input_device == "key":
-		if((mouse_coords).length() < 70):
-			global_position = player.global_position+mouse_coords
+		if((test_mouse_coords - player.global_position).length() < 70):
+			global_position = test_mouse_coords
 		else:
-			global_position = player.global_position + (direction * 70)
+			global_position = player.global_position + (test_direction * 70)
 	else:
 		position = (crosshair_direction * 50)
