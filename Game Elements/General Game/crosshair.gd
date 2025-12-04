5084cdd1c39408dfef8b72426bb942c1ba3d1a9c
ZE873 <ettersoz@oregonstate.edu>
2025-12-04 13:12:43 -0800

    updated clamp

--- Game Elements/General Game/crosshair.gd
+++ Game Elements/General Game/crosshair.gd
@@ -26,26 +26,25 @@ func _process(_delta: float) -> void:
 		input_direction = Input.get_vector("look_left_" + player_input_device, "look_right_" + player_input_device, "look_up_" + player_input_device, "look_down_" + player_input_device).normalized()
 		if(input_direction != Vector2(0,0)):
 			crosshair_direction = input_direction
-			
 	
-	var camera = get_tree().get_root().get_node("LayerManager/game_container/game_viewport/game_root/Camera2D")
-	var viewport_size = get_tree().get_root().get_node("LayerManager/game_container/game_viewport").size
-	var mouse_pos_viewport = get_tree().get_root().get_mouse_position()
-	var mouse_coords = camera.get_global_transform().affine_inverse().basis_xform(mouse_pos_viewport - Vector2(viewport_size/2))
-	var direction = (mouse_coords).normalized()
+	var camera = get_viewport().get_camera_2d()
+	var mouse_coords = camera.get_global_mouse_position()
+	var direction  = (mouse_coords - player.global_position).normalized()
 	
-	mouse_coords = mouse_coords * mouse_sensitivity
-	const CIRCLE_RADIUS = 70
+	var CIRCLE_RADIUS = 70
 	
 	if player_input_device == "key":
-		if((mouse_coords).length() < CIRCLE_RADIUS):
-			global_position = player.global_position + mouse_coords
+		if((mouse_coords - player.global_position).length() < CIRCLE_RADIUS):
+			global_position = mouse_coords
 		else:
+			
 			var clamped_offset = direction * CIRCLE_RADIUS
 			global_position = player.global_position + clamped_offset
 			
-			var offset_unscaled = clamped_offset / mouse_sensitivity
-			var viewport_clamped = camera.get_global_transform().basis_xform(offset_unscaled) + Vector2(viewport_size/2)
-			Input.warp_mouse(viewport_clamped)
+			var unscaled_offset = clamped_offset / mouse_sensitivity
+			var target_mouse_world = player.global_position + unscaled_offset
+			
+			var screen_pos = camera.get_viewport().get_screen_transform() * camera.get_canvas_transform() * target_mouse_world
+			Input.warp_mouse(screen_pos)
 	else:
 		position = (crosshair_direction * 50)
