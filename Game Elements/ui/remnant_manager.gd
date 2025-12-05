5d927bcc756eccc3ce6d7a67a1f28aaac5767112
Sheparry <sheparry@oregonstate.edu>
2025-12-05 02:03:41 -0800

    OMG FINALLY

--- Game Elements/ui/remnant_manager.gd
+++ Game Elements/ui/remnant_manager.gd
@@ -8,19 +8,26 @@ func _ready():
 
 #Loads all resources from res://remnants/
 func _load_all_remnants() -> void:
-	var dir = DirAccess.open("res://Game Elements/Remnants/")
+	#var dir = DirAccess.open("res://Game Elements/Remnants/")
+	var dir = ResourceLoader.list_directory("res://Game Elements/Remnants/")
+
 	if dir == null:
 		push_error("Remnants folder not found: rres://Game Elements/Remnants/")
 		return
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		if not dir.current_is_dir() and file_name.ends_with(".tres"):
-			var res = ResourceLoader.load("res://Game Elements/Remnants/" + file_name)
+	#dir.list_dir_begin()
+	#var file_name = dir.get_next()
+	
+	for file in dir:
+		if file.ends_with(".tres"):
+			var res = ResourceLoader.load("res://Game Elements/Remnants/" + file)
 			if res:
 				remnant_pool.append(res)
-		file_name = dir.get_next()
-	dir.list_dir_end()
+		#if not dir.current_is_dir() and file_name.ends_with(".tres"):
+		#	var res = ResourceLoader.load("res://Game Elements/Remnants/" + file_name)
+		#	if res:
+		#		remnant_pool.append(res)
+		#file_name = dir.get_next()
+	#dir.list_dir_end()
 
 #Returns an array of up to `num` unique random remnants from the pool.
 func get_random_remnants(num: int = 4, player1_remnants: Array = [], player2_remnants : Array = []) -> Array[Resource]:
@@ -29,8 +36,8 @@ func get_random_remnants(num: int = 4, player1_remnants: Array = [], player2_rem
 		return result
 
 	#Split the count
+	@warning_ignore("integer_division")
 	var half := int(num / 2)
-
 	#Filter pools
 	var pool_for_p1: Array = []
 	var pool_for_p2: Array = []
@@ -38,19 +45,15 @@ func get_random_remnants(num: int = 4, player1_remnants: Array = [], player2_rem
 	#Arrays of remnant names
 	var p1_names: Array = []
 	var p2_names: Array = []
-
 	for r in player1_remnants:
 		p1_names.append(r.remnant_name)
-
 	for r in player2_remnants:
 		p2_names.append(r.remnant_name)
-
 	for rem in remnant_pool:
 		if rem.remnant_name not in p1_names:
 			pool_for_p1.append(rem)
 		if rem.remnant_name not in p2_names:
 			pool_for_p2.append(rem)
-
 	# Pick half from each
 	_pick_random_unique(pool_for_p1, half, result)
 	_pick_random_unique(pool_for_p2, half, result)
