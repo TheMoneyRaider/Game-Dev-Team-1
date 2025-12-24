902fcdcaaf3014dddff26262c51641a0c9cc5231
TheMoneyRaider <83470534+TheMoneyRaider@users.noreply.github.com>
2025-12-24 13:50:58 -0800

    Moving target made

---
+++ Game Elements/Objects/moving_target.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var range: float = 48
+@export var speed: float = 48
+@export var wander_strength := 5
+@export var center_pull := 1.5
+
+var origin: Vector2
+var velocity: Vector2
+
+func _ready():
+	origin = position
+	velocity = Vector2.RIGHT.rotated(randf() * TAU)
+
+func _process(delta):
+	#Small random wander
+	velocity = velocity.rotated(randf_range(-wander_strength, wander_strength) * delta)
+
+	#Soft pull toward center
+	var to_center = origin - position
+	var dist = to_center.length()
+	if dist > 0.01:
+		var pull = to_center.normalized() * (dist / range) * center_pull
+		velocity += pull * delta
+
+	velocity = velocity.normalized()
+	position += velocity * speed * delta
